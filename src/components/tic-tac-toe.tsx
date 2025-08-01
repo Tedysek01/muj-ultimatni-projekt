1dc7b7cb3e65f628a1ad14faa27442cbe199f534
Tedysek01 <tadeas@raska.eu>
2025-08-01 17:48:22 +0200

    Updated from Naklikam.cz

--- src/components/tic-tac-toe.tsx
+++ src/components/tic-tac-toe.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { ArrowLeft, RotateCcw, Trophy, Users } from 'lucide-react'
 
